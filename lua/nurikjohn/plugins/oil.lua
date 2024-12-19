c18145b93a1cb4a66dbe739c34ef566cf3d4d977
Nuriddin <nmuhammadjanov@gmail.com>
2024-12-20 01:23:32 +0500

    custom oil bar & alpha.nvim

--- lua/nurikjohn/plugins/oil.lua
+++ lua/nurikjohn/plugins/oil.lua
@@ -7,7 +7,15 @@ return {
 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
 	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 	config = function()
+		CustomOilBar = function()
+			local path = vim.fn.expand("%")
+			path = path:gsub("oil://", "")
+
+			return "  " .. vim.fn.fnamemodify(path, ":.")
+		end
+
 		require("oil").setup({
+			columns = { "icon" },
 			keymaps = {
 				["g?"] = "actions.show_help",
 				["<CR>"] = "actions.select",
@@ -33,6 +41,9 @@ return {
 				["gx"] = "actions.open_external",
 				["g."] = "actions.toggle_hidden",
 				["g\\"] = "actions.toggle_trash",
+			},
+			win_options = {
+				winbar = "%{v:lua.CustomOilBar()}",
 			},
 		})
 
