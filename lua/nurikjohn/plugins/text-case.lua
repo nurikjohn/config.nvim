bacdac5929f21a30db22f23c59e5d2659f5c46d5
Nuriddin <nmuhammadjanov@gmail.com>
2025-07-16 16:19:08 +0500

    lsp change text case

--- lua/nurikjohn/plugins/text-case.lua
+++ lua/nurikjohn/plugins/text-case.lua
@@ -1,21 +1,44 @@
 return {
 	"johmsalas/text-case.nvim",
-	dependencies = { "nvim-telescope/telescope.nvim" },
 	config = function()
 		require("textcase").setup({
 			default_keymappings_enabled = true,
 			prefix = "~",
 		})
-		require("telescope").load_extension("textcase")
 
 		require("textcase.extensions.whichkey").register_prefix("n", "~", "Change text case")
+
+		local map = vim.keymap.set
+		map(
+			"n",
+			"~u",
+			"<cmd>lua require('textcase').lsp_rename('to_upper_case')<CR>",
+			{ desc = "LSP Rename to UPPER_CASE" }
+		)
+		map(
+			"n",
+			"~l",
+			"<cmd>lua require('textcase').lsp_rename('to_lower_case')<CR>",
+			{ desc = "LSP Rename to lower_case" }
+		)
+		map(
+			"n",
+			"~s",
+			"<cmd>lua require('textcase').lsp_rename('to_snake_case')<CR>",
+			{ desc = "LSP Rename to snake_case" }
+		)
+		map(
+			"n",
+			"~c",
+			"<cmd>lua require('textcase').lsp_rename('to_camel_case')<CR>",
+			{ desc = "LSP Rename to camelCase" }
+		)
+		map(
+			"n",
+			"~p",
+			"<cmd>lua require('textcase').lsp_rename('to_pascal_case')<CR>",
+			{ desc = "LSP Rename to PascalCase" }
+		)
 	end,
-	cmd = {
-		"Subs",
-		"TextCaseOpenTelescope",
-		"TextCaseOpenTelescopeQuickChange",
-		"TextCaseOpenTelescopeLSPChange",
-		"TextCaseStartReplacingCommand",
-	},
 	lazy = false,
 }
