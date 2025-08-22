957a7ce994d578300d89fb091bb6eae8d3a25943
Nuriddin <nmuhammadjanov@gmail.com>
2025-08-22 12:18:19 +0500

    theme switcher

--- lua/nurikjohn/plugins/color-scheme.lua
+++ lua/nurikjohn/plugins/color-scheme.lua
@@ -1,53 +1,8 @@
-return {
-	-- {
-	-- 	"nurikjohn/anysphere",
-	-- 	dir = "/Users/nurik/Documents/code/config/anysphere",
-	-- 	name = "anysphere",
-	-- 	config = function()
-	-- 		-- require("anysphere").setup({})
-	-- 		-- require("anysphere").load()
-	-- 	end,
-	-- },
-	-- {
-	-- 	"navarasu/onedark.nvim",
-	-- 	name = "onedark",
-	-- 	config = function()
-	-- 		-- require("onedark").setup({
-	-- 		-- style = "warmer",
-	-- 		-- })
-	-- 		-- require("onedark").load()
-	-- 	end,
-	-- },
-	-- {
-	-- 	"rose-pine/neovim",
-	-- 	name = "rose-pine",
-	-- 	config = function()
-	-- 		-- vim.cmd("colorscheme rose-pine")
-	-- 	end,
-	-- },
-	-- {
-	-- 	"kdheepak/monochrome.nvim",
-	-- 	name = "monochrome",
-	-- 	config = function()
-	-- 		-- vim.cmd("colorscheme monochrome")
-	-- 	end,
-	-- },
-	-- {
-	-- 	"projekt0n/github-nvim-theme",
-	-- 	name = "github-theme",
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		-- vim.cmd("colorscheme github_dark")
-	-- 	end,
-	-- },
-	{
-		"kepano/flexoki-neovim",
-		name = "flexoki",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd("colorscheme flexoki-dark")
-		end,
-	},
-}
+local current_theme_path = vim.fn.expand("~/.config/themes/current-theme/nvim.lua")
+
+if vim.fn.filereadable(current_theme_path) == 1 then
+	return dofile(current_theme_path)
+else
+	-- Fallback to default theme
+	return require("nurikjohn.themes.default-theme")
+end
